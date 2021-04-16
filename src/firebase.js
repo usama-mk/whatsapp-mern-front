c8c52786b275598596690ffa4647131c0013f323
Usama Muhammad <usamamuhammad45678@gmail.com>
2021-04-16 15:33:22 +0500

    front structure

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjiZqYq_kaPGHsntR8yh-FprD7VAZPGz4",
+    authDomain: "whatsapp-clone-mern-c8845.firebaseapp.com",
+    projectId: "whatsapp-clone-mern-c8845",
+    storageBucket: "whatsapp-clone-mern-c8845.appspot.com",
+    messagingSenderId: "627865549066",
+    appId: "1:627865549066:web:f93caafb7a1afaee2c22f3"
+  };
